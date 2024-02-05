b5f2d68ec4a85f15f56f94ec9b2c216e522530a4
kzkzsk <www.k.sakuma@gmail.com>
2024-02-05 13:13:49 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,5 +3,6 @@
     テスト1
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
-    <x-tests.card title="タイトル"/>
+    <x-tests.card title="タイトル" />
+    <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
