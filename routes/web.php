1bcbc463568ea4b44921b6d691f91229a02df855
kzkzsk <www.k.sakuma@gmail.com>
2024-02-05 18:16:50 +0900

    feat: サービスプロバイダーのテスト

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,6 @@ Route::middleware('auth')->group(function () {
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainertest', [LifecycleTestController::class, 'showServiceContainerTest']);
+Route::get('/serviceprovidertest', [LifecycleTestController::class, 'showServiceProviderTest']);
 
 require __DIR__.'/auth.php';
