c4dfc06d748a4b1f0a0c9a00f363904be6bbef45
kzkzsk <www.k.sakuma@gmail.com>
2024-02-16 14:58:49 +0900

    Shop追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             AdminSeeder::class,
-            OwnerSeeder::class
+            OwnerSeeder::class,
+            ShopSeeder::class
         ]);
     }
 }
