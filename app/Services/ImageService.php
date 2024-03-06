00ebd728b45191fd6b373f838698ef8d4a3f7c02
kzkzsk <www.k.sakuma@gmail.com>
2024-03-06 12:56:37 +0900

    shop edit, update作成、画像保存処理の修正

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -11,9 +11,11 @@ class ImageService
         //
         // 本来はここにInterventionImageのリサイズ処理を入れて、画像リサイズ処理を汎用化したい。
         //
+        $fileName = uniqid((rand().'_'));
+        $extension = $imageFile->extension();
+        $fileNameToStore = $fileName . '.' . $extension;
+        Storage::putFileAs('public/' . $folderName . '/', $imageFile, $fileNameToStore); // リサイズなしの場合
 
-        Storage::putFile('public/' . $folderName . '/', $imageFile); // リサイズなしの場合
-
-        return $imageFile;
+        return $fileNameToStore;
     }
 }
